ba8b07e74bbb5f9a908e63f8996066e5ce7bcfc1
Stereowalker <stereowalker.mc@gmail.com>
2021-05-27 05:10:23 +0100

    Fix blocks not retaining their properties when ageing

--- src/main/java/com/stereowalker/burdenoftime/mixin/ServerWorldMixin.java
+++ src/main/java/com/stereowalker/burdenoftime/mixin/ServerWorldMixin.java
@@ -14,9 +14,11 @@ import com.stereowalker.burdenoftime.conversions.AgeConversion;
 import com.stereowalker.burdenoftime.conversions.Conversions;
 import com.stereowalker.burdenoftime.world.AgeMap;
 
+import net.minecraft.block.AbstractBlock.Properties;
 import net.minecraft.block.BlockState;
 import net.minecraft.profiler.IProfiler;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.state.Property;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.chunk.Chunk;
@@ -26,15 +28,16 @@ import net.minecraft.world.server.ServerWorld;
 @Mixin(ServerWorld.class)
 public abstract class ServerWorldMixin
 {
-    @Shadow
-    public abstract ServerWorld getWorld();
+	@Shadow
+	public abstract ServerWorld getWorld();
 
-    @Inject(at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/chunk/ChunkSection;getBlockState(III)Lnet/minecraft/block/BlockState;", ordinal = 0), method = "tickEnvironment(Lnet/minecraft/world/chunk/Chunk;I)V", locals = LocalCapture.CAPTURE_FAILHARD)
-    public void tickEnvironment(Chunk chunkIn, int randomTickSpeed, CallbackInfo ci, ChunkPos chunkpos, boolean flag, int i, int j, IProfiler iprofiler, ChunkSection var8[], int var9, int var10, ChunkSection chunksection, int k, int l, BlockPos blockpos1, BlockState blockstate)
-    {
-    	ageBlock(getWorld(), blockstate, new Random(), blockpos1);
-    }
+	@Inject(at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/chunk/ChunkSection;getBlockState(III)Lnet/minecraft/block/BlockState;", ordinal = 0), method = "tickEnvironment(Lnet/minecraft/world/chunk/Chunk;I)V", locals = LocalCapture.CAPTURE_FAILHARD)
+	public void tickEnvironment(Chunk chunkIn, int randomTickSpeed, CallbackInfo ci, ChunkPos chunkpos, boolean flag, int i, int j, IProfiler iprofiler, ChunkSection var8[], int var9, int var10, ChunkSection chunksection, int k, int l, BlockPos blockpos1, BlockState blockstate)
+	{
+		ageBlock(getWorld(), blockstate, new Random(), blockpos1);
+	}
 
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	private void ageBlock(ServerWorld world, BlockState blockstate, Random rand, BlockPos pos)
 	{
 		MinecraftServer server = world.getServer();
@@ -55,7 +58,12 @@ public abstract class ServerWorldMixin
 		{
 			if (conversion.from == blockstate.getBlock() && currentAge > conversion.requiredAge)
 			{
-				world.setBlockState(pos, conversion.to.getDefaultState(), 11);
+				BlockState convertedState = conversion.to.getDefaultState();
+				for (Property property : blockstate.getProperties()) {
+					if (convertedState.hasProperty(property))
+						convertedState = convertedState.with(property, blockstate.get(property));
+				}
+				world.setBlockState(pos, convertedState, 11);
 			}
 		}
 	}
