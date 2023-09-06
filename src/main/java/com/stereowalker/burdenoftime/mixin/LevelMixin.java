1bf04a28954ebd7f0b72370175b5ca46e9841247
Stereowalker <stereowalker.mc@gmail.com>
2023-09-06 20:35:06 +0100

    Fix mixin errors

--- src/main/java/com/stereowalker/burdenoftime/mixin/LevelMixin.java
+++ src/main/java/com/stereowalker/burdenoftime/mixin/LevelMixin.java
@@ -19,7 +19,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 
 @Mixin(Level.class)
-public abstract class WorldMixin
+public abstract class LevelMixin
 {
     @Shadow
     public abstract boolean isClientSide();
