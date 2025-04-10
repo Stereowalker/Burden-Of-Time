2aa36483ee6bcad8bfe7a86d9779658bc8bf7a0f
Stereowalker <stereowalker.mc@gmail.com>
2025-04-10 10:42:29 +0100

    Fix NPE

--- src/main/java/com/stereowalker/burdenoftime/world/AgeErosionMap.java
+++ src/main/java/com/stereowalker/burdenoftime/world/AgeErosionMap.java
@@ -33,8 +33,11 @@ public class AgeErosionMap extends SavedData
     {
         for (BlockPos entry : ageMap.keySet())
         {
-            int age = ageMap.get(entry);
-            tag.putInt(gson.toJson(entry), age);
+        	if (ageMap.get(entry) != null) 
+        	{
+        		int age = ageMap.get(entry);
+        		tag.putInt(gson.toJson(entry), age);
+        	}
         }
         return tag;
     }
