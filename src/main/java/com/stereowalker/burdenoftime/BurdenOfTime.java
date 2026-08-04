ea9fa76520787fa1337075b13e25314504eeddde
Stereowalker <stereowalker.mc@gmail.com>
2026-08-04 22:26:35 +0100

    Added an intermediate bock between grass and dirt

--- src/main/java/com/stereowalker/burdenoftime/BurdenOfTime.java
+++ src/main/java/com/stereowalker/burdenoftime/BurdenOfTime.java
@@ -5,9 +5,17 @@ import com.stereowalker.burdenoftime.conversions.Conversions;
 import com.stereowalker.burdenoftime.resource.ConversionDataManager;
 import com.stereowalker.unionlib.api.collectors.ConfigCollector;
 import com.stereowalker.unionlib.api.collectors.ReloadListeners;
+import com.stereowalker.unionlib.api.registries.RegistryCollector;
+import com.stereowalker.unionlib.core.registries.RegistryHolder;
+import com.stereowalker.unionlib.core.registries.RegistryObject;
 import com.stereowalker.unionlib.mod.MinecraftMod;
 import com.stereowalker.unionlib.mod.ServerSegment;
+import com.stereowalker.unionlib.util.VersionHelper;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.GrassBlock;
 import net.neoforged.fml.common.Mod;
 
 @Mod(value = "burdenoftime")
@@ -40,6 +48,17 @@ public class BurdenOfTime extends MinecraftMod {
 
 	public static BurdenOfTime getInstance() {
 		return instance;
+	}
+	
+	@Override
+	public void setupRegistries(RegistryCollector collector) {
+		collector.addRegistryHolder(Registries.BLOCK, BlockRegis.class);
+	}
+	
+	@RegistryHolder(namespace = ID)
+	public static class BlockRegis {
+		@RegistryObject("patchy_grass_block_1")
+		public static final Block transformed = new GrassBlock(VersionHelper.copyBlockPropertyWithId(Blocks.GRASS_BLOCK, ID+":patchy_grass_block_1"));
 	}
 
 }
