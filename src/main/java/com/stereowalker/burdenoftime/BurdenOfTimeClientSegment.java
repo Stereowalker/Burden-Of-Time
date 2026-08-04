3b806f0271241d00d1cd9bb524134cce2fdaafe6
Stereowalker <stereowalker.mc@gmail.com>
2026-08-04 22:26:35 +0100

    Added an intermediate bock between grass and dirt

--- src/main/java/com/stereowalker/burdenoftime/BurdenOfTimeClientSegment.java
+++ src/main/java/com/stereowalker/burdenoftime/BurdenOfTimeClientSegment.java
@@ -1,11 +1,15 @@
 package com.stereowalker.burdenoftime;
 
+import java.util.List;
+
 import com.stereowalker.burdenoftime.config.Config;
+import com.stereowalker.unionlib.api.collectors.ColorOverrideCollector;
 import com.stereowalker.unionlib.client.gui.screens.config.ConfigScreen;
 import com.stereowalker.unionlib.mod.ClientSegment;
 import com.stereowalker.unionlib.util.VersionHelper;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.color.block.BlockTintSources;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
@@ -16,10 +20,15 @@ public class BurdenOfTimeClientSegment extends ClientSegment {
 	public Identifier getModIcon() {
 		return VersionHelper.toLoc("burdenoftime","textures/icon.png");
 	}
-	
+
 	@Override
 	public Screen getConfigScreen(Minecraft mc, Screen previousScreen) {
 		return new ConfigScreen(previousScreen, Config.class, Component.translatable("burdenoftime.gui.config"));
+	}
+
+	@Override
+	public void setupColorOverrides(ColorOverrideCollector collector) {
+		collector.overrideBlocks(List.of(BlockTintSources.grassBlock()), BurdenOfTime.BlockRegis.transformed);
 	}
 
 }
